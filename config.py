920356a54fb815768f1c822fe4aaff88a69b1fa3
kamaldhankhar <kamaldhankhararts@gmail.com>
2025-02-19 22:22:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -72,7 +72,7 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://vault.pictures/p/a857f893c2144247b923f06e7888d337"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://vault.pictures/p/a857f893c2144247b923f06e7888d337"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
